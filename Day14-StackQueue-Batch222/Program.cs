45de17488cbfe5fec2740874f57dea82a0ec2bbe
Aneesh <katta.aneesh597@gmail.com>
2022-11-30 12:18:14 +0530

    added program

---
+++ Day14-StackQueue-Batch222/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day14_StackQueue_Batch222
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Stack Elements:");
+            LinkedListStack linkedListStack = new LinkedListStack();
+            linkedListStack.Push(70);
+            linkedListStack.Push(30);
+            linkedListStack.Push(56);
+            linkedListStack.Display();
+        }
+    }
+}
